c11b5af805ee861c0a3432454097f4675002ee82
Tommy Phan <tommy.phan@my.simpson.edu>
2020-08-27 15:35:08 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-Hi
+print("Elspeth Tirel continued to be plagued by the nightmares brought upon her by Ashiok, the Dream Render.")
+print("In her nightmares, she dreams of Heliod, the Sun-Crowned, betraying her and sending her to the Therian underworld.")
+print("Plagued with the recurrent nightmares of Heliod's Sun Spear, Khrusor, and due to Ashiok's dream renders, Elspeth Tirel's dreams becomes reality.")
+print("Elspeth Tirel conquers death and brings into manifestationion her own spear, the \"Shadowspear\".")
